f36d8811482a9ed22e90eac8273b04e847422741
jwlee101526 <jwlee101526@kakao.com>
2026-05-21 13:00:18 +0900

    감정 분석 모델 코드 수정

--- huggingface/src/sentiment.py
+++ huggingface/src/sentiment.py
@@ -1,48 +1,69 @@
-import re
 import gradio as gr
+import pandas as pd
 from transformers import pipeline
 
 classifier = pipeline(
     task="sentiment-analysis",
-    model="tabularisai/multilingual-sentiment-analysis"
+    model="tabularisai/multilingual-sentiment-analysis",
+    top_k=None,
 )
 
-LABEL_MAPPINGS = {
-    "Very Positive": "긍정",
-    "Positive": "긍정",
-    "Negative": "부정"
-}
 
-def predict_sentiment(text: str) -> str:
+def predict_sentiment(text: str):
     if not text or not text.strip():
-        return "텍스트를 입력해주세요."
+        return {}, None
 
     try:
-        result = classifier(text)[0]
-        raw_label = result["label"]
-        score = result["score"]
+        results = classifier(text)[0]
 
-        final_label = LABEL_MAPPINGS.get(raw_label, raw_label)
+        label_map = {
+            "Very Positive": "매우 긍정 😁",
+            "Positive": "긍정 😊",
+            "Neutral": "중립 😐",
+            "Negative": "부정 🤨",
+            "Very Negative": "매우 부정 😞",
+        }
 
-        return f"감정 : {final_label}, 점수 : {score:.4f}"
+        sentiment_data = [
+            {"감정": label_map.get(res["label"], res["label"]), "점수": res["score"]}
+            for res in results
+        ]
+
+        label_scores = {d["감정"]: d["점수"] for d in sentiment_data}
+        df = pd.DataFrame(sentiment_data)
+
+        return label_scores, df
 
     except Exception as e:
-        return f"분석 중 오류가 발생했습니다: {str(e)}"
+        print(e)
+        return {}, None
 
-app = gr.Interface(
-    fn=predict_sentiment,
-    title="AI 감정분석 웹앱",
-    description="HuggingFace Transformers를 사용한 모델 기반 감정분석",
-    inputs=[gr.Text(
+
+with gr.Blocks() as app:
+    gr.Markdown("# AI 감정분석 웹앱")
+    gr.Markdown("HuggingFace Transformers를 사용한 모델 기반 감정분석")
+
+    input_text = gr.Textbox(
         label="Text", 
-        lines=3,
-        placeholder="문장을 입력하세요",
-    )],
-    outputs=[gr.Text(
-        label="Result",
-        lines=3
-    )],
-)
+        lines=3, 
+        placeholder="문장을 입력하세요"
+    )
+
+    btn = gr.Button("분석하기")
+
+    with gr.Row():
+        text_output = gr.Label(label="Result")
+
+        plot_output = gr.BarPlot(
+            value=None, 
+            x="감정", 
+            y="점수", 
+            label="chart", 
+            tooltip=["감정", "점수"]
+        )
+
+    btn.click(predict_sentiment, inputs=input_text, outputs=[text_output, plot_output])
+
 
 if __name__ == "__main__":
     app.launch(debug=True)
